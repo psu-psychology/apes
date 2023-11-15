33f392be07e4b700a90e3aff57e19a858f58192a
rogilmore <rog1@psu.edu>
2023-11-15 08:37:33 -0500

    Archive old versions.

--- app.R
+++ app.R
@@ -10,94 +10,72 @@
 source("R/util.R")
 
 ui <- fluidPage(
+  
   # Application title
   titlePanel("Alpha, Power, Effect Sizes, Sample Size"),
   
-  # Row 0
-  fluidRow(column(
-    2,
-    actionButton("regenerateData", "Regenerate",
-                 style = "color: #fff; background-color: #337ab7; border-color: #2e6da4"),
-    offset = 1
-  ),),
-  
-  # Row 1
   fluidRow(
-    column(1,
-           # "A",
-           numericInput(
-             "sample_size_a", "n_A", value = 75, min = 1
-           )),
-    column(1,
-           # "Criterion & Effect Size",
-           numericInput(
-             "alpha",
-             label = greeks("alpha"),
-             value = 0.05,
-             step = .001
-           )),
-    column(1,
-           # "B",
-           numericInput(
-             "sample_size_b", "n_B", value = 75, min = 1
-           ))
-  ),
-  
-  # Row 2
-  fluidRow(
-    column(1,
-           numericInput(
-             "mean_a",
-             paste0(greeks("mu"), "_A"),
-             value = 0,
-             step = 0.1
-           )),
-    column(1,
-           numericInput(
-             "effect_size",
-             label = "d",
-             value = 0.5,
-             step = 0.1
-           )),
     column(
       2,
-      paste0(greeks("mu"), "_B = ", greeks("mu"), "_A + d", "\n\n"),
-      textOutput("out_text")
-    )
-  ),
-  
-  # Row 3
-  fluidRow(column(
-    1,
-    numericInput(
-      "sd_a",
-      paste0(greeks("sigma"), "_A"),
-      value = 1,
-      min = 0.1,
-      step = 0.1
-    )
-  ),
-  column(
-    1,
-    numericInput(
-      "sd_b",
-      paste0(greeks("sigma"),
-             "_B"),
-      value = 1,
-      min = 0.1,
-      step = 0.1
-    )
-  )),
-  
-  # Row 4
-  fluidRow(
-    column(2, "t test", verbatimTextOutput("ttest")),
+      numericInput("sample_size_a", "Sample size (n_A)", value = 75, min = 1),
+      numericInput(
+        "mean_a",
+        paste0("Mean (", greeks("mu"), "_A)"),
+        value = 0,
+        step = 0.1
+      ),
+      numericInput(
+        "sd_a",
+        paste0("Std Dev (", greeks("sigma"), "_A)"),
+        value = 1,
+        min = 0.1,
+        step = 0.1
+      ),
+      hr(), # horizontal rule
+      numericInput(
+        "alpha",
+        label = greeks("alpha"),
+        value = 0.05,
+        step = .005
+      ),
+      checkboxInput("paired_samples", "Paired Samples", value = FALSE)
+    ),
     column(2,
-           "Power",
-           verbatimTextOutput("power")),
-    column(8, label = "Histogram", plotOutput("histPlot"))
+           numericInput("sample_size_b", "Sample size (n_B)", value = 75, min = 1),
+           numericInput(
+             "effect_size",
+             label = "Effect size (d)",
+             value = 0.5,
+             step = 0.1
+           ),
+           numericInput(
+             "sd_b",
+             paste0("Std Dev (", greeks("sigma"), "_B)"),
+             value = 1,
+             min = 0.1,
+             step = 0.1)
+    ),
+    column(
+      8,
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Histogram", plotOutput("histPlot")),
+        tabPanel("Violin-Box", plotOutput("histBoxPlot")),
+        tabPanel("Power curve", plotOutput("powerPlot"))
+      )
+    )
+  ),
+  fluidRow(
+    column(4,
+           actionButton("regenerateData", "Regenerate",
+                        style = "color: #fff; background-color: #337ab7; border-color: #2e6da4")),
+    column(3, skip = 1,
+           verbatimTextOutput("ttest")),
+    column(3, verbatimTextOutput("power"))
   )
 )
+
+#---------------------------------------------
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
@@ -123,7 +101,7 @@ server <- function(input, output, session) {
     reactive({
       input$regenerateData
       rnorm(n = input$sample_size_b,
-            mean = mean_b(),
+            mean = mean_b(), # reactive calculated as sum of mean_a + effect_size
             sd = input$sd_b)
     })
   
@@ -134,11 +112,19 @@ server <- function(input, output, session) {
            xlim = c(-5, 5))
   }, res = 96)
   
+  output$histBoxPlot <- renderPlot({
+    my_box_violin(sample_a(),
+           sample_b(),
+           binwidth = .5,
+           xlim = c(-5, 5))
+  }, res = 96)
+  
   output$ttest <- renderText({
     t_test(sample_a(),
            sample_b(),
            alpha(),
-           var.equal = (input$sd_a == input$sd_b))
+           var.equal = (input$sd_a == input$sd_b),
+           paired = paired_test())
   })
   
   mean_b <- reactive({
@@ -152,14 +138,28 @@ server <- function(input, output, session) {
   mu_b <- reactive(mean(sample_b()))
   alpha <- reactive(input$alpha)
   
+  paired_test <- reactive({
+    input$paired_samples
+  })
+
   output$power <- renderText({
-    t_test_power(length(sample_a()), length(sample_b()), d())
+    t_test_power(n1 = input$sample_size_a, 
+                 n2 = input$sample_size_b, 
+                 d = d(),
+                 sig.level = alpha())
   })
   
   
-  # output$powerPlot <- renderPlot({
-  #   t_test_power_plot(length(sample_a()), d())
-  # })
+  
+  
+  output$powerPlot <- renderPlot({
+    t_test_power_plot(
+      n1 = input$sample_size_a,
+      n2 = input$sample_size_b,
+      d = d(),
+      sig.level = alpha()
+    )
+  })
 }
 
 # Run the application
